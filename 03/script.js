0ce432c7b20c199462d10a6885cadaa2ffca57cc
jatin-rwt <rawatjatin0206@gmail.com>
2024-10-08 15:02:57 +0530

    03

---
+++ 03/script.js
@@ -0,0 +1,37 @@
+//number
+let num1 = 3;
+let num2 = 2.3;
+console.log(num1 + num2);
+
+//string
+//can do both single or double  quotes
+let firstName = 'Jatin';
+let secondName = 'Rawat';
+let id = '123@qwerty';
+console.log(firstName);
+console.log(secondName);
+console.log(firstName+" "+secondName);
+
+//boolean
+let isLoggedIn = true;
+console.log(isLoggedIn);
+
+//null -> any
+let lastLogin = null;
+console.log(lastLogin);
+
+//undefined -> any
+let age = undefined;
+console.log(age);
+
+//Object
+//{ key value pairs seperated by commas }
+const person = {
+    firstName: "Dark",
+    secondName: "Knight",
+    age: 22,
+    isLoggedIn: true,
+}
+console.log(person);
+console.log(person.firstName);
+console.log(person.secondName);
