d6d5bdadad7d02f4cfb013d4eed42e12a4066426
jatin-rwt <rawatjatin0206@gmail.com>
2024-10-08 16:13:08 +0530

    05

---
+++ 05/script.js
@@ -0,0 +1,22 @@
+const age = 20;
+const sex = "male";
+
+if(age>=18 && sex == 'male'){
+    console.log('you are a man');
+    
+}
+
+if(age>=18 || sex == 'male'){
+    console.log('you have XY chromosomes');
+    
+}
+
+const number = 5;
+
+if(number%2 != 0){
+    console.log("odd");
+    
+} else {
+    console.log("even");
+    
+}
