a8e7e36178fc1bb80af1bcffc0459ae21dbe815e
jatin-rwt <rawatjatin0206@gmail.com>
2024-10-09 13:37:58 +0530

    12 Dom

---
+++ 12/script.js
@@ -0,0 +1,18 @@
+alert('welcome!!')
+
+
+let username = prompt("enter your name", "voldemort")
+console.log("hello " + username);
+
+console.log(window);
+
+
+console.log(document.title);
+console.log(document.body);
+
+//manipulate the dom
+document.title = "DOM manipulation"
+
+console.log(document.title);
+
+document.write("thanks for visiting " + username);
