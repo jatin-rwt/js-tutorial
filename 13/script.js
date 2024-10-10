0deeb164af062d4fa81cf2a26a09ac905a29fcbc
jatin-rwt <rawatjatin0206@gmail.com>
2024-10-10 14:03:14 +0530

    getElementsByClassName

--- 13/script.js
+++ 13/script.js
@@ -1,15 +1,8 @@
-const el = document.getElementById("username");
-console.log(el);
-console.log(el.innerText);
+const elements = document.getElementsByClassName("block");
+console.log(elements);
 
-el.innerText = "Tyrion Lannister"
-el.classList.add("red-color", "underline");//adds red-color to the el
-console.log(el.classList);
+for(let i = 0; i < elements.length; i++){
+    //console.log(elements.item(i));
+    elements.item(i).style.background = "blue";
+}
 
-el.classList.remove("dashed");
-console.log(el.classList);
-el.style.textDecoration = "underline";
-el.style.color = "blue";
-
-el.parentElement.style.background = "grey";
-console.log(el);
